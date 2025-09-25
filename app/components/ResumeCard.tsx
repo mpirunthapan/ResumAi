f8abd9813164988360cd6d5c97b7d492d0c0df98
mpirunthapan <mpirunthapan@gmail.com>
2025-09-25 20:59:19 +0530

    Authentication

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 import ScoreCircle from './ScoreCircle'
 
-const ResumeCard: ({ resume }: { resume: Resume }) => Element = ({ resume: {id, companyName, jobTitle, feedback, imagePath, resumePath} }: { resume: Resume }) => {
+const ResumeCard: ({ resume }: { resume: Resume }) => React.ReactElement = ({ resume: {id, companyName, jobTitle, feedback, imagePath, resumePath} }: { resume: Resume }) => {
   return (
     <Link to={`resume/${id}`} className='resume-card animate-in fade-in duration-1000'>
         <div className='resume-card-header'>
